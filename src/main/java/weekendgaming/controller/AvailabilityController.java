8bb6e5018584b3fcfb9de31b715640c4c5b4a0cb
YashMohey <49582793+YashMohey@users.noreply.github.com>
2021-02-26 12:01:12 +0530

    Logger + YML added.

--- src/main/java/weekendgaming/controller/AvailabilityController.java
+++ src/main/java/weekendgaming/controller/AvailabilityController.java
@@ -13,12 +13,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
 import weekendgaming.dto.AvailabilityDetailsRequestDTO;
 import weekendgaming.model.Availability;
 import weekendgaming.service.AvailabilityService;
 
 @RestController
 @RequestMapping("/api")
+@Slf4j
 public class AvailabilityController
 {
 
@@ -28,31 +30,35 @@ public class AvailabilityController
 	@GetMapping("/availability")
 	public List<Availability> getFutureAvailability()
 	{
+		log.info("Request for Availability Details");
 		return availabilityService.getFutureAvailability();
 	}
-	
+
 	@GetMapping("/availability/days/{noOfDays}")
 	public List<Availability> getFutureAvailability(@PathVariable("noOfDays") int noOfDays)
 	{
+		log.info("Request for Availability Details for the next " + noOfDays + " days");
 		return availabilityService.getFutureAvailability(noOfDays);
 	}
 
 	@PostMapping("/availability")
 	public void addAvailability(@RequestBody AvailabilityDetailsRequestDTO availabilityDetails)
 	{
+		log.info("Adding new Availability Detail - " + availabilityDetails.toString());
 		availabilityService.addAvailability(availabilityDetails);
 	}
 
 	@DeleteMapping("/availability/{id}")
 	public void deleteAvailability(@PathVariable("id") long id)
 	{
+		log.info("Deleting availability with ID - " + id);
 		availabilityService.deleteAvailability(id);
 	}
 
 	@ExceptionHandler(ConstraintViolationException.class)
 	public void handleException(ConstraintViolationException e)
 	{
-		System.out.println("Constraint Violation Exception - Multiple Entries");
+		log.warn("Constraint Violation Exception - Multiple Entries");
 	}
 
 }
--- src/main/java/weekendgaming/service/impl/AvailabilityServiceImpl.java
+++ src/main/java/weekendgaming/service/impl/AvailabilityServiceImpl.java
@@ -9,6 +9,7 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import lombok.extern.slf4j.Slf4j;
 import weekendgaming.dto.AvailabilityDetailsRequestDTO;
 import weekendgaming.model.Availability;
 import weekendgaming.repository.AvailabilityRepository;
@@ -16,6 +17,7 @@ import weekendgaming.service.AvailabilityService;
 
 @Service
 @Transactional
+@Slf4j
 public class AvailabilityServiceImpl implements AvailabilityService
 {
 
@@ -25,13 +27,14 @@ public class AvailabilityServiceImpl implements AvailabilityService
 	@Override
 	public void addAvailability(AvailabilityDetailsRequestDTO availabilityDetails)
 	{
-		System.out.println(availabilityDetails);
-
 		OffsetDateTime start = OffsetDateTime.parse(availabilityDetails.getAvailableFrom());
 		OffsetDateTime end = OffsetDateTime.parse(availabilityDetails.getAvailableTill());
 
 		if (start.isAfter(end))
+		{
+			log.warn("User End time was after start time. Details : " + availabilityDetails);
 			return;
+		}
 
 		Availability availability = new Availability();
 
@@ -39,69 +42,54 @@ public class AvailabilityServiceImpl implements AvailabilityService
 		availability.setAvailableFrom(start);
 		availability.setAvailableTill(end);
 
-		availabilityRepository.save(availability);
+		availability = availabilityRepository.save(availability);
+
+		log.info("Availability Added - " + availability.toString());
 	}
 
 	@Override
 	public List<Availability> getFutureAvailability()
 	{
-		OffsetDateTime currTime = OffsetDateTime.now();
-
-		System.out.println("Current Time = " + currTime.toString());
-
-		List<Availability> validAvailability = new ArrayList<>();
-
-		List<Availability> allAvailability = availabilityRepository.findAll();
-		System.out.println("allAvailability : " + allAvailability.toString());
-
-		for (Availability availability : availabilityRepository.findAll())
-		{
-			System.out.println("User : " + availability.getName() + " | Time : " + availability.getAvailableFrom() + " - " + availability.getAvailableTill());
-
-			// Delete Old Dates and Clear Database
-			if (availability.getAvailableTill().isBefore(currTime))
-				availabilityRepository.delete(availability);
-			// Check if anyone starts playing in the future
-			else
-				validAvailability.add(availability);
-		}
-
-		System.out.println("Valid Availability = " + validAvailability.toString());
-		return validAvailability;
+		return getFutureAvailability(0);
 	}
 
 	@Override
 	public List<Availability> getFutureAvailability(int noOfDays)
 	{
 		OffsetDateTime currTime = OffsetDateTime.now();
-
-		System.out.println("Current Time = " + currTime.toString());
-
-		List<Availability> validAvailability = new ArrayList<>();
+		log.debug("Fetching Details. Current Time = " + currTime.toString() + " | No of Days = " + noOfDays);
 
 		List<Availability> allAvailability = availabilityRepository.findAll();
-		System.out.println("allAvailability : " + allAvailability.toString());
+		log.debug("All Details : " + allAvailability.toString());
 
+		List<Availability> response = new ArrayList<>();
 		for (Availability availability : availabilityRepository.findAll())
 		{
-			System.out.println("User : " + availability.getName() + " | Time : " + availability.getAvailableFrom() + " - " + availability.getAvailableTill());
+			log.debug("User : " + availability.getName() + " | Time : " + availability.getAvailableFrom() + " - " + availability.getAvailableTill());
 
 			// Delete Old Dates and Clear Database
 			if (availability.getAvailableTill().isBefore(currTime))
+			{
+				log.debug("Deleting outdated detail : " + availability.toString());
 				availabilityRepository.delete(availability);
-			// Check if anyone starts playing in the next 2 days
-			else if (availability.getAvailableFrom().isBefore(currTime.plusDays(noOfDays)))
-				validAvailability.add(availability);
+			}
+
+			// Check if anyone starts playing in the future no of days
+			else if (noOfDays == 0 || availability.getAvailableFrom().isBefore(currTime.plusDays(noOfDays)))
+			{
+				response.add(availability);
+			}
 		}
 
-		System.out.println("Valid Availability = " + validAvailability.toString());
-		return validAvailability;
+		log.info("Availability Details : " + response.toString());
+		return response;
 	}
 
 	@Override
 	public void deleteAvailability(long id)
 	{
 		availabilityRepository.deleteById(id);
+		log.info("Availability with ID: " + id + " deleted");
 	}
 
 }
